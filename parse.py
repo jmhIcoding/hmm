7a28e2c70500ea4dda534d53f404a07f6536f183
jmhIcoding <1024836002@qq.com>
2018-10-25 08:24:18 +0800

    更新 添加 训练集 验证集的划分

--- parse.py
+++ parse.py
@@ -2,12 +2,17 @@ __author__ = 'jmh081701'
 import json
 import os
 import re
+import  random
 WORD=0
 POS=1
 exp =0.000000000001 #无穷小量 防止出现分母为0
 class Text_loader(object):
-    def __init__(self,file):
+    def __init__(self,file,seprate_rate=0.2):
         self.file =file
+        self.seprate_rate = seprate_rate
+        self.A={} #转移频次字典{'本词的词性':{'下个词的词性i':次数;···'total':总次数}···}
+        self.B={} #发射频次字典{'当前词性':{'发射状态i':次数,···,'total':总次数}}
+        self.PI={}#初始状态频次字典
 
     def load_text(self):
         with open(self.file,encoding='utf8') as fp :
@@ -28,16 +33,17 @@ class Text_loader(object):
                         print(each)
                 rawdata[i].append((word,pos))
                 #每一行,多个(词,词性)对
-
-        self.rawdata =  rawdata
-        self.A={} #转移频次字典{'本词的词性':{'下个词的词性i':次数;···'total':总次数}···}
-        self.B={} #发射频次字典{'当前词性':{'发射状态i':次数,···,'total':总次数}}
-        self.PI={}#初始状态频次字典
+        #划分训练集,验证集
+        len_lines = len(rawdata)
+        valid_lines = int(len_lines * self.seprate_rate)
+        start_pointer = random.randint(0,len_lines-valid_lines-1) #随机取出一堆来作为验证集
+        self.traindata = rawdata[0:start_pointer]+rawdata[(start_pointer+valid_lines):]
+        self.validdata = rawdata[start_pointer:(start_pointer+valid_lines)]
 
     def generator(self):
         #基于语料库,使用极大似然法得到各种参数
         len_lines = 0
-        for line in self.rawdata:
+        for line in self.traindata:
             len_lines += 1
             len_line= len(line)
             for i in range(len_line):
@@ -112,6 +118,8 @@ class Text_loader(object):
     def save(self,model_path="hmm.model"):
         with open(model_path,"w") as fp:
             json.dump({'A':self.A,'B':self.B,'PI':self.PI},fp)
+        with open(model_path+".valid","w") as fp:
+            json.dump({'valid_data':self.validdata},fp)
 
     def load(self,model_path="hmm.model"):
         with open(model_path) as fp:
@@ -119,11 +127,13 @@ class Text_loader(object):
             self.A = _['A']
             self.B = _['B']
             self.PI = _['PI']
+        with open(model_path+".valid") as fp:
+            _ = json.load(fp)
+            self.validdata = _['valid_data']
+        return self.A,self.B,self.PI,self.validdata
+
 if __name__ == '__main__':
     loader = Text_loader(file="data/raw_data.txt")
     loader.load_text()
-    A,B,PI=loader.generator()
-    print(PI)
-    print(B)
-    print(A)
+    loader.generator()
     loader.save()
